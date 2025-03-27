2b7166217b1df8b841b1e89210476a827076b0e5
am-sazid <amsazid.cse@gmail.com>
2025-03-27 12:56:56 +0600

    Input-in-c

--- first_c.c
+++ first_c.c
@@ -1,8 +1,29 @@
-# include<stdio.h>
+// # include<stdio.h>
 
-int main (){
+// int main (){
+//     int age = 18;
+//     float pi = 3.14;
+//     char star = '*';
+//     printf("%d\n",age);
+//     printf("%f\n",pi);
+//     printf("%c\n",star);
+//     return 0;
+// }
 
-    char number[] = "Sazid";
-    printf("%s\n",number);
+#include<stdio.h>
+
+int main() {
+
+    int a,b;
+    printf("Enter a : ");
+    scanf("%d", &a);
+
+    printf("Enter b : ");
+    scanf("%d", &b);
+
+    int sum = a + b;
+
+    printf("The Sum : %d\n",sum);
+
     return 0;
 }
