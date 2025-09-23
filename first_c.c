e137804e6c2e52c13811009ba4b0a663d8df88f4
am-sazid <amsazid.cse@gmail.com>
2025-09-23 23:07:52 +0600

    Trying C Programming

--- first_c.c
+++ first_c.c
@@ -1,29 +1,11 @@
-// # include<stdio.h>
+# include<stdio.h>
 
-// int main (){
-//     int age = 18;
-//     float pi = 3.14;
-//     char star = '*';
-//     printf("%d\n",age);
-//     printf("%f\n",pi);
-//     printf("%c\n",star);
-//     return 0;
-// }
-
-#include<stdio.h>
-
-int main() {
-
-    int a,b;
-    printf("Enter a : ");
-    scanf("%d", &a);
-
-    printf("Enter b : ");
-    scanf("%d", &b);
-
-    int sum = a + b;
-
-    printf("The Sum : %d\n",sum);
-
+int main (){
+    int age = 18;
+    float pi = 3.14;
+    char star = '*';
+    printf("%d\n",age);
+    printf("%f\n",pi);
+    printf("%c\n",star);
     return 0;
 }
