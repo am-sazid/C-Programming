9f3a65a74b7aacd573b8ad77141227034ffa9e09
am-sazid <amsazid.cse@gmail.com>
2025-09-25 20:52:52 +0600

    Test

--- first_c.c
+++ first_c.c
@@ -1,11 +1,15 @@
-# include<stdio.h>
+#include <stdio.h>
 
-int main (){
+int main()
+{
     int age = 18;
+    int age2 = 18;
     float pi = 3.14;
     char star = '*';
-    printf("%d\n",age);
-    printf("%f\n",pi);
-    printf("%c\n",star);
+    printf("%d\n", age);
+    printf("%d\n", age2);
+
+    printf("%f\n", pi);
+    printf("%c\n", star);
     return 0;
 }
