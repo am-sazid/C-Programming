be0e57b49725cdef73cbcd6e391997f2450b8f5f
am-sazid <amsazid.cse@gmail.com>
2025-10-11 20:36:44 +0600

    Update

---
+++ Module 4/Assignment 01/problem02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int A ;
+    int B ;
+    scanf("%d %d",&A , &B);
+    long long multi = A * B;
+    printf("%lld\n",multi); 
+    return 0;
+}
