5ca0f89dbb5d4effa66580aa44bf793d251034da
am-sazid <amsazid.cse@gmail.com>
2025-11-05 01:02:39 +0600

    Shape 3

--- Practice Module/Module 14.5/W_Shape_3.c
+++ Practice Module/Module 14.5/W_Shape_3.c
@@ -4,22 +4,26 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int star = 0;
-    int star2 = n;
-    int space = n - 1;
+
+    // Upper half
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= space; j++)
-        {
+        for (int j = i; j < n; j++)
             printf(" ");
-        }
-        for (int s = 0; s <= star; s++)
-        {
+        for (int k = 1; k <= (2 * i - 1); k++)
             printf("*");
-        }
         printf("\n");
-        space--;
-        star += 2;
     }
+
+    // Lower half
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = i; j < n; j++)
+            printf(" ");
+        for (int k = 1; k <= (2 * i - 1); k++)
+            printf("*");
+        printf("\n");
+    }
+
     return 0;
 }
